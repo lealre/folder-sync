552624fc597d07ca51316c008c0bac10e6aed659
lealre <renaanvp@gmail.com>
2024-06-08 16:02:08 +0100

    refactor: change structure

--- app/__main__.py
+++ app/__main__.py
@@ -3,14 +3,11 @@ from arg_parser import parse_args
 
 @parse_args
 def main(args):
-    sync_folders = FolderSync(
+    FolderSync(
         source_path=args.source_path,
         replica_path=args.replica_path,
         log_path=args.log_path,
         sync_time=args.sync_time
-    )
+    ).start_sync()
 
-    sync_folders.start_sync()
-
-if __name__ == '__main__':
-    main()  
+main()
